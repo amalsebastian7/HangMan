6062031230814b87d40807306eb9553a82949a9e
Amal sebastian <amalpsebastian98@gmail.com>
2022-06-26 14:05:14 +0100

    Updated call

--- hangman/hangman_Template.py
+++ hangman/hangman_Template.py
@@ -25,10 +25,6 @@ class Hangman:
         while self.num_lives >0:
             if letter in list(self.word):
                 self.num_letters-=1
-                #index= [ i for i in range(len(self.word)) if self.word[i] == letter ]
-                #index=[self.word.find(letter) for x in ]
-                #index=[letter.index() for letter in self.word ]
-                
                 index= [i for i,L in enumerate (self.word) if L == letter]
                 self.word_guessed[index]=letter
                 print(f"The word is :{self.word_guessed}")
@@ -37,25 +33,8 @@ class Hangman:
                 print(f"The letter guessed is wrong")
             break
         pass
-'''
-
-Checks if the letter is in the word.
-If it is, it replaces the '_' in the word_guessed list with the letter.
-If it is not, it reduces the number of lives by 1.
-Parameters:
-----------
-letter: str
-The letter to be checked
-
-'''
-        # TODO 3: Check if the letter is in the word. TIP: You can use the lower() method to convert the letter to lowercase
-        # TODO 3: If the letter is in the word, replace the '_' in the word_guessed list with the letter
-        # TODO 3: If the letter is in the word, the number of UNIQUE letters in the word that have not been guessed yet has to be reduced by 1
-        # TODO 3: If the letter is not in the word, reduce the number of lives by 1
-        # Be careful! A letter can contain the same letter more than once. TIP: Take a look at the index() method in the string class
 
     def ask_letter(self):
-
          while True:
             letter =input("Enter a letter: ").lower()
             if len(letter) != 1:
@@ -69,34 +48,10 @@ The letter to be checked
             break
     pass
 
-            '''Asks the user for a letter and checks two things:
-            1. If the letter has already been tried
-            2. If the character is a single character
-            If it passes both checks, it calls the check_letter method.
-            '''
-
-        # TODO 1: Ask the user for a letter iteratively until the user enters a valid letter
-        # TODO 1: Assign the letter to a variable called `letter`
-        # TODO 1: The letter has to comply with the following criteria: It has to be a single character. If it is not, print "Please, enter just one character"
-        # TODO 2. It has to be a letter that has not been tried yet. Use the list_letters attribute to check this. If it has been tried, print "{letter} was already tried".
-        # TODO 3: If the letter is valid, call the check_letter method
 
 def play_game(word_list):
     game = Hangman(word_list, num_lives=5)
     game.ask_letter()
-
-    pass
-
-    
-    # As an aid, part of the code is already provided:
-    
-    # TODO 1: To test this task, you can call the ask_letter method
-    # TODO 2: To test this task, upon initialization, two messages should be printed 
-    # TODO 3: To test this task, you call the ask_letter method and check if the letter is in the word
-    
-    # TODO 4: Iteratively ask the user for a letter until the user guesses the word or runs out of lives
-    # If the user guesses the word, print "Congratulations, you won!"
-    # If the user runs out of lives, print "You ran out of lives. The word was {word}"
 
 if __name__ == '__main__':
     word_list = [ "Execution","Tea","Supposedly", "there" ,"are" ,"over" ,"one","million", "words" ,"in", "the" ,"English" ,"Language"   ]
