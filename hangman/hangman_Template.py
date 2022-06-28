1502cd8e50945a9017968319772a24a86233fd33
Amal Sebastian <83443068+amalsebastian7@users.noreply.github.com>
2022-06-28 15:45:13 +0100

    Completed the basic  hangman project

--- hangman/hangman_Template.py
+++ hangman/hangman_Template.py
@@ -1,3 +1,100 @@
+import random
+
+#create class 
+class Hangman:
+#defining all initializations
+
+    def __init__(self, word_list, num_lives):
+        self.word_list = word_list
+        self.num_lives = 3
+        self.word = list(random.choice(self.word_list).lower())
+        self.list_letter=[]
+        self.word_guessed =['_']*len(self.word)
+        self.num_letters=len(set(self.word))
+
+        print(f"The mystery word has:  {len(self.word)} characters")
+        print(f"{self.word_guessed}")
+        pass
+
+    def check_letter(self, letter) -> None:
+        if letter in list(self.word):
+            self.num_letters -= 1
+            for i, L in enumerate(self.word):
+                if L == letter:
+                    self.word_guessed[i] = L
+            print(f"Yes {letter} is in the word")
+            print(f"Your word so far : {self.word_guessed}")
+        else:
+            self.num_lives -=1
+            print(f"The letter guessed is wrong,you have :  {self.num_lives} lives left")
+            print(f"Your word so far : {self.word_guessed}")
+        pass
+
+    def ask_letter(self):
+        while True:
+            letter =input("Enter a letter: ").lower()
+            if len(letter) != 1:
+                    print(f"Enter just a character,Please: ")
+                    continue
+            elif letter in self.list_letter:
+                    print(f"{letter}  already tried")
+                    self.list_letter.append(letter) 
+                    continue 
+            else:
+                self.check_letter(letter)
+            break
+        return letter 
+    pass
+
+
+def play_game(word_list):
+    game = Hangman(word_list, num_lives=5) 
+    while game.num_lives > 0:
+        game.ask_letter() 
+        if game.num_lives == 0:
+            print(f"The word is : {game.word}")
+            print(f"You lost")
+            break
+        elif game.num_letters == 0: 
+            print("Congratulations, you won!") 
+            break 
+if __name__ == '__main__':
+    word_list = [ "Execution","Tea","Supposedly", "there" ,"are" ,"over" ,"one","million", "words" ,"in", "the" ,"English" ,"Language"]
+    play_game(word_list)
+
+word = 'apple'
+num_letters=len(set(word))
+print(num_letters)
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+'''
+Previous code:
+
 from dataclasses import replace
 import random
 
@@ -65,4 +162,6 @@ def play_game(word_list):
 if __name__ == '__main__':
     word_list = [ "Execution","Tea","Supposedly", "there" ,"are" ,"over" ,"one","million", "words" ,"in", "the" ,"English" ,"Language"   ]
     play_game(word_list)
+
+    '''
 # %%
